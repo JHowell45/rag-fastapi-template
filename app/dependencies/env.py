bf1b1784d27bc9098a294b0c4a3177aaae01a433
= <jhowell45@googlemail.com>
2025-05-14 21:44:41 +0100

    setting up the actual envs variables and defaults

--- app/dependencies/env.py
+++ app/dependencies/env.py
@@ -1,10 +1,23 @@
 from typing import Annotated
 
 from fastapi import Depends
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    #######################################
+    # Ollama
+    #######################################
+    OLLAMA_HOST: str = Field(default="ollama")
+    OLLAMA_PORT: int = Field(default=11434)
+
+    #######################################
+    # ChromaDB
+    #######################################
+    CHROMADB_HOST: str = Field(default="vdb")
+    CHROMADB_PORT: int = Field(default=8000)
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
