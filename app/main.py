c71ea397288c231f2de45ae082282be115f24625
= <jhowell45@googlemail.com>
2025-05-15 13:18:13 +0100

    adding templating

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.api import router
 
 app = FastAPI()
 app.include_router(router)
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/", include_in_schema=False)
-def redirect_to_docs() -> RedirectResponse:
-    return RedirectResponse(url="/docs")
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def redirect_to_docs(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request=request, name="index.html", context={})
